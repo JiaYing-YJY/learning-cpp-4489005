9e9076a6e5de4ea75e4438c43cf49d5aa0bff1ac
JiaYing-YJY <84889593+JiaYing-YJY@users.noreply.github.com>
2024-04-24 20:36:42 +0000

    update

--- src/Ch02/02_12b/CodeDemo.cpp
+++ src/Ch02/02_12b/CodeDemo.cpp
@@ -5,14 +5,24 @@
 #include <iostream>
 #include <cstdint>
 
-enum cow_purpose {dairy, meat, hide, pet};
+//enum constants are public, static and final (unchangeable - cannot be overridden) (variable to be a set of pre-defined constants)
+enum class cow_purpose {dairy, meat, hide, pet}; //enum classes is encapsulated within a class scope, enhances type-safety, reduce potentional errors
+//enum work like global variables
+enum class grocery_section {canned, frozen, meat, laundry, dairy, bakery};
+// both enum has meat and bakery -> has two values => change to enum class => need to use scope resolution operator, double colon
 
 int main(){
+    int meat = 8; //expected behaviour to print a = 8 for multiple scopes -> local variable => assume it means the one in the scope
+    /*
     int a;
-
-    a = meat;
-
+    a = (int) cow_purpose::meat;
     std::cout << "a = " << a << std::endl;
+    */
+
+    cow_purpose a;
+    a = cow_purpose::meat;
+
+    std::cout << "a = " << (int) a << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
