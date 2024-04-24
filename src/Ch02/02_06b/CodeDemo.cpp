81b55abe86a468aedbc79346f78cc89d95512dd6
JiaYing-YJY <84889593+JiaYing-YJY@users.noreply.github.com>
2024-04-24 19:32:42 +0000

    update

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -2,10 +2,23 @@
 // Exercise 02_06
 // Preprocessor directives, by Eduardo Corpeño 
 
-#include <iostream>
+#include <iostream> //insert reference file into code 
+#include <string>
+#include <cstdint> //C code
+#define CAPACITY 5000 //find and replace (macro definitions)
+#define DEBUG
 
 int main(){
-    
+    int32_t large = CAPACITY;
+    uint8_t small = 37;
+#ifdef DEBUG 
+//checks for the existence of macro definitions
+//If the identifier specified is defined as a macro, the lines of code that immediately follow the condition are passed on to the compiler
+    std::cout << "[About to perform the addition]" << std::endl;
+#endif
+    large += small;
+    std::cout << "The large integer is " << large << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
--- src/Ch02/02_08b/CodeDemo.cpp
+++ src/Ch02/02_08b/CodeDemo.cpp
@@ -4,8 +4,30 @@
 
 #include <iostream>
 
+//#define AGE_LENGTH 4 //macro has no scope
+
+//preprocessor directives do not need a semicolon
+
 int main(){
-    
+    const size_t AGE_LENGTH = 4; //recommended alternative to macro
+    //size_t useful when dealing with arrays and memory blocks, as sizes can never be negative
+    int age[AGE_LENGTH];
+    float temperature[] = {31.5, 32.7, 38.90}; //auto does not work for arrays so type must be specified
+
+    age[0] = 25;
+    age[1] = 20;
+    age[2] = 19;
+    age[3] = 19;
+    std::cout << "The Age array has " << AGE_LENGTH << " elements." << std::endl;
+    std::cout << "Age[0] = " << age[0] << std::endl;
+    std::cout << "Age[1] = " << age[1] << std::endl;
+    std::cout << "Age[2] = " << age[2] << std::endl;
+    std::cout << "Age[3] = " << age[3] << std::endl;
+    std::cout << std::endl;
+    std::cout << "Temp[0] = " << temperature[0] << std::endl;
+    std::cout << "Temp[1] = " << temperature[1] << std::endl;
+    std::cout << "Temp[2] = " << temperature[2] << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
